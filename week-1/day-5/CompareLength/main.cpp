0f9a27b4ff5dfc8663ee04c04f996edb95c73a8b
91bcsabi <c.balazsi91@gmail.com>
2019-09-13 10:30:12 +0200

    practiceofArrays

---
+++ week-1/day-5/CompareLength/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // - Create an array variable named `p1`
+    //   with the following content: `[1, 2, 3]`
+    // - Create an array variable named `p2`
+    //   with the following content: `[4, 5]`
+    // - Print "p2 is longer" if `p2` has more elements than `p1`
+
+    int p1[] = {1, 2, 3};
+    int p2[] = {4, 5};
+
+    if(int i = 0;sizeof(p1) < sizeof(p2)){
+        cout << "p2 is longer" << endl;
+    }
+
+    return 0;
+}
