f60b8fc3ce0086301d7da023b87adb0929b6053c
91bcsabi <c.balazsi91@gmail.com>
2019-09-13 10:12:45 +0200

    practicofArrays

---
+++ week-1/day-5/Third/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    // - Create an array variable named `q`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print the third element of `q
+
+    int q[] = {4, 5, 6, 7};
+
+    std::cout << "The third element is: " << q[2] << std::endl;
+
+    return 0;
+}
